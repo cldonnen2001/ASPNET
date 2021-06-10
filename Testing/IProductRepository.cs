083276cfdf4dbe3c6a10d6396c94210e9700cd6d
Christine Godfrey <cldonnen2001@yahoo.com>
2021-06-09 21:13:26 -0500

    completed delete button (#5)

--- Testing/IProductRepository.cs
+++ Testing/IProductRepository.cs
@@ -10,7 +10,10 @@ namespace Testing
     {
         public IEnumerable<Product> GetAllProducts();// stubbed out method = no scope/body
         public Product GetProduct(int id);
-                public void UpdateProduct(Product product);
-
+        public void UpdateProduct(Product product);
+        public void InsertProduct(Product productToInsert);
+        public IEnumerable<Category> GetCategories();
+        public Product AssignCategory();
+        public void DeleteProduct(Product product);
     }
 }
